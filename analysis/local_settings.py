823a82ff9e49e81fa076fe6332a864dcc2124fec
Iana Vasylieva <iana.vasylieva@gmail.com>
2023-02-13 16:08:27 -0500

    Remove local settings from gitignore

---
+++ analysis/local_settings.py
@@ -0,0 +1,19 @@
+import json
+
+
+def update_settings(settings, config_file):
+    with open(config_file, "r") as f:
+        config = json.load(f)
+        settings.PYTORCH_MODEL_PATH = config.get("PYTORCH_MODEL_PATH")
+        settings.PYTORCH_MODEL_NAME = config.get("PYTORCH_MODEL_NAME")
+        settings.DB_LOCATION = config.get("DB_LOCATION")
+        settings.PYTORCH_MODEL_VERSION = config.get("PYTORCH_MODEL_VERSION")
+        settings.DATAFRAME_VERSION = config.get("DATAFRAME_VERSION")
+
+    # print("Settings file", config_file)
+    # with open(config_file, "r") as f:
+    #     local_settings_str = f.read()
+    #     local_settings = json.loads(local_settings_str)
+    #     print("local_settings", local_settings)
+    # for k, v in local_settings.items():
+    #     setattr(settings, k, v)
