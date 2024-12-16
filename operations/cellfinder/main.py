ea40e942f50db19e9392251c73083500dcc32743
Iana Vasylieva <iana.vasylieva@gmail.com>
2024-12-16 11:00:49 -0500

    Transition cellfinder on tiff series

--- operations/cellfinder/main.py
+++ operations/cellfinder/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import subprocess
 import time
@@ -6,6 +7,7 @@ from glob import glob
 from imaris_ims_file_reader import ims
 
 from ..base import ImageOperation
+from analysis import settings
 
 
 class cellfinder(ImageOperation):
@@ -17,68 +19,45 @@ class cellfinder(ImageOperation):
         "input": "/h20/Public/cakir-i/4CL16/chow1_mag8x_montage.ims",
         "output": "/h20/Public/cakir-i/4CL16/analysis/chow1_mag8x_montage",
         "operation": "cellfinder",
-        "extras": {
-            "signal_channel": 0,
-            "resolution_level": 0
-        }
+        "extras": {}
     }
     """
     def __init__(self, input, output, **kwargs):
         super().__init__(input, output, **kwargs)
-        self.signal_channel = int(kwargs.get('signal_channel', 0))
-        self.resolution_level = int(kwargs.get('resolution_level', 0))
+        self.metadata = json.load(open(os.path.join(self.input, f'.{settings.INFO_FILE_NAME}'), 'r'))
+        self.signal_channel = int(self.metadata['channel'])
+        self.resolution_level = int(self.metadata['resolution_level'])
 
         self.output_operation_folder = os.path.join(self.output, 'cellfinder')
         self.jobs_folder = os.path.join(self.output_operation_folder, "slurm_jobs")
-        self.detection_folder = os.path.join(self.output_operation_folder, f"cellfinder_channel_{self.signal_channel}")
-        self.ims_file = ims(self.input)
-        self.resolution = self.ims_file.metaData[(self.resolution_level, 0, self.signal_channel, 'resolution')][-3:]
+        self.detection_folder = os.path.join(
+            self.output_operation_folder,
+            f"resolution_level_{self.resolution_level}",
+            f"channel_{self.signal_channel}",
+            f"cellfinder_output"
+        )
+        # self.ims_file = ims(self.input)
+        # self.resolution = self.ims_file.metaData[(self.resolution_level, 0, self.signal_channel, 'resolution')][-3:]
+        self.resolution = self.metadata['resolution']
         if not os.path.exists(self.jobs_folder):
             os.makedirs(self.jobs_folder)
         if not os.path.exists(self.detection_folder):
             os.makedirs(self.detection_folder)
 
     def run(self):
-        print("Running brainreg")
+        print("Running cellfinder")
         print("Input", self.input)
         print("Output", self.output)
         print("Channel", self.signal_channel)
-        self.stack_to_detect = os.path.join(self.output_operation_folder, f'resolution_level_{self.resolution_level}', f'channel_{self.signal_channel}')
-        self.extract_tiff_series()  # extract tiff series in SLURM
-        z_layers = self.ims_file.metaData[(self.resolution_level, 0, self.signal_channel, 'shape')][-3]
-        extracted_files = glob(os.path.join(self.output_operation_folder, f'resolution_level_{self.resolution_level}', f'channel_{self.signal_channel}', "*.tif"))
-        while len(extracted_files) < z_layers:
-            extracted_files = glob(os.path.join(self.output_operation_folder, f'resolution_level_{self.resolution_level}', f'channel_{self.signal_channel}', "*.tif"))
-            print(f"Extracted files: {len(extracted_files)} of {z_layers}")
-            time.sleep(10)
+        # self.stack_to_detect = os.path.join(self.output_operation_folder, f'resolution_level_{self.resolution_level}', f'channel_{self.signal_channel}')
+        # self.extract_tiff_series()  # extract tiff series in SLURM
+        # z_layers = self.ims_file.metaData[(self.resolution_level, 0, self.signal_channel, 'shape')][-3]
+        # extracted_files = glob(os.path.join(self.output_operation_folder, f'resolution_level_{self.resolution_level}', f'channel_{self.signal_channel}', "*.tif"))
+        # while len(extracted_files) < z_layers:
+        #     extracted_files = glob(os.path.join(self.output_operation_folder, f'resolution_level_{self.resolution_level}', f'channel_{self.signal_channel}', "*.tif"))
+        #     print(f"Extracted files: {len(extracted_files)} of {z_layers}")
+        #     time.sleep(10)
         self.run_detection()  # run brainreg in SLURM
-        # delete tiff series  # TODO
-
-    def extract_tiff_series(self):
-        z_layers = self.ims_file.metaData[(self.resolution_level, 0, self.signal_channel, 'shape')][-3]
-        path_to_task = os.path.join(self.jobs_folder, f"extract_imaris_z_layers_rl{self.resolution_level}_c{self.signal_channel}.sh")
-        main_script = os.path.abspath(__file__)
-        slurm_script = os.path.join(os.path.dirname(main_script), "extract_imaris_z_layer.py")
-        with open(path_to_task, 'w') as f:
-            f.write('#!/bin/bash\n')
-            f.write("source /h20/home/lab/miniconda3/bin/activate peace")
-            f.write('\n')
-            f.write(f'python {slurm_script}')
-            f.write(' ')
-            f.write(str(self.input))
-            f.write(' ')
-            f.write(str(self.output))
-            f.write(' ')
-            f.write(str(self.resolution_level))
-            f.write(' ')
-            f.write(str(self.signal_channel))
-            f.write(' ')
-            f.write('$SLURM_ARRAY_TASK_ID')
-            f.write('\n')
-
-        #### run it on compute (cpu) partition
-        command = ['sbatch', f'--array=0-{z_layers-1}', '-p', 'compute', '--mem=32Gb', '-n12', path_to_task]
-        subprocess.run(command)
 
     def run_detection(self):
         path_to_task = os.path.join(self.jobs_folder, f"cellfinder_rl{self.resolution_level}_c{self.signal_channel}.sh")
@@ -88,7 +67,12 @@ class cellfinder(ImageOperation):
             # f.write('\n')
             f.write("source /h20/home/lab/miniconda3/bin/activate cellfinder")
             f.write('\n')
-            f.write(f'cellfinder -s {self.stack_to_detect} -b {self.stack_to_detect} -o {self.detection_folder}')
+            f.write('cellfinder -s ')
+            f.write(self.input if ' ' not in self.input else f'"{self.input}"')
+            f.write(' -b ')
+            f.write(self.input if ' ' not in self.input else f'"{self.input}"')
+            f.write(' -o ')
+            f.write(self.detection_folder if ' ' not in self.detection_folder else f'"{self.detection_folder}"')
             f.write(f' -v {str(self.resolution[0])} {str(self.resolution[1])} {str(self.resolution[2])}')
             f.write(f' --orientation sal --atlas allen_mouse_25um')
             f.write(f' --no-analyse --no-figures --no-register --no-classification --ball-z-size {str(int(self.resolution[0])+1)}')
