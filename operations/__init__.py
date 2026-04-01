92be48ede50a51701d48c2a216487c183edae245
Iana <33125830+noisysky@users.noreply.github.com>
2026-04-01 13:20:19 -0400

    Add gaussian blur operation OC (#8)

--- operations/__init__.py
+++ operations/__init__.py
@@ -4,6 +4,7 @@ from .cellfinder import cellfinder
 from .ants import ants
 from .stretch_contrast import stretch_contrast
 from .adaptive_histogram_equalization import adaptive_histogram_equalization
+from .gaussian_blur import gaussian_blur
 from .ilastik import ilastik
 from .resnet_classification import resnet_classification
 from .cellpose import cellpose
