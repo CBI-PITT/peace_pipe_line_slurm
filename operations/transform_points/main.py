e1cafc20223d3bc1a4cdb791b8076d76b60a6561
Iana <33125830+noisysky@users.noreply.github.com>
2026-05-14 14:59:55 -0400

    Generate binary atlas-space tiff after transforming points (#12)

--- operations/transform_points/main.py
+++ operations/transform_points/main.py
@@ -36,6 +36,7 @@ class transform_points(ImageOperation):
         self.jobs_folder = os.path.join(output_folder_sequence, "slurm_jobs")
         self.results_folder = output_folder_sequence
         self.out_csv_path = os.path.join(self.results_folder, f"{os.path.basename(self.cells_path.replace('.csv', ''))}_for_dashboard.csv")
+        self.out_binary_tiff_path = os.path.join(self.results_folder, f"{os.path.basename(self.cells_path.replace('.csv', ''))}_for_dashboard_binary.tiff")
         self.prerequisites = kwargs.get('prerequisites', [])
         print("Prerequisites", self.prerequisites)
         os.umask(settings.UMASK)
@@ -47,10 +48,10 @@ class transform_points(ImageOperation):
     def run(self):
         provenance_file_path = self.create_provenance()
         job_ids = []
-        if not os.path.exists(self.out_csv_path):
+        if not os.path.exists(self.out_csv_path) or not os.path.exists(self.out_binary_tiff_path):
             job_ids = self.get_df()
         else:
-            print("Output CSV file already exists")
+            print("Output CSV file and binary TIFF already exist")
         return provenance_file_path, job_ids
 
     def create_provenance(self):
